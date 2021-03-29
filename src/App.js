1ebaf2c300b93b1f5534715d26318880e6fc2c2e
Eric LaFontsee <elafontsee@gmail.com>
2021-03-29 11:19:08 -0500

    added boz shadow to resume

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
-import Home from "./pages/Home";
 import Header from "./components/Header";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Testimonials from "./pages/Testimonials";
+import Resume from "./pages/Resume";
+import "./style.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
- 
     <>
-    <Header/>
-    <Home/>
+      <Header />
+      <BrowserRouter>
+        <div className="container-fluid mainContent">
+          <Switch>
+            <Route exact path={"/"} component={About} />
+            <Route exact path={"/projects"} component={Projects} />
+            <Route exact path={"/contact"} component={Contact} />
+            <Route exact path={"/testimonials"} component={Testimonials} />
+            <Route exact path={"/resume"} component={Resume} />
+          </Switch>
+        </div>
+        <div className="container-fluid polygon"></div>
+      </BrowserRouter>
     </>
   );
 }
